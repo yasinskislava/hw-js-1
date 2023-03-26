b054bf36e054b89c07c843114283cbf68321d087
yasinskislava <yasinskislava@gmail.com>
2023-03-26 10:06:40 +0300

    hw2 fix

--- js/script6.js
+++ js/script6.js
@@ -1,5 +1,5 @@
 const name = 'MacBook Pro 13"';
 let price = 10000;
-console.log("Обрано", name, ", ціна за штуку 10000 кредитів");
+console.log("Обрано", name, ", ціна за штуку", price, "кредитів");
 price = 20000;
-console.log("Обрано", name, ", ціна за штуку 20000 кредитів");
+console.log("Обрано", name, ", ціна за штуку", price, "кредитів");
