95695c3bba7307cea13472dfe7f6e31fdbfe0692
yasinskislava <yasinskislava@gmail.com>
2023-04-27 18:36:15 +0300

    hw6 update

--- js/script3.js
+++ js/script3.js
@@ -4,12 +4,12 @@ const number = 45;
 if (number < x1) {
     console.log("Це число знаходиться до x1");
 }
-if (number > x1) {
+else if (number > x1) {
     console.log("Це число знаходиться після x1");
 }
-if (number >= x1 && number <= x2) {
+else if (number >= x1 && number <= x2) {
     console.log("Це число входить у відрізок між x1 та x2");
 }
-if (number < x1 || number > x2) {
+else {
     console.log("Це число не входить у відрізок між x1 та x2");
 }
