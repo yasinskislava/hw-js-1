4df41edd200256c6cd394e2e4c5828db7f2a0d45
yasinskislava <yasinskislava@gmail.com>
2023-08-16 22:05:06 +0300

    hw21

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-// Отримати загальну суму балансу (поле balance) всіх користувачів.
+// Створення почти з імені й фамілії0
 
-const users = [
-    { userId: 1, balance: 200 },
-    { userId: 2, balance: 120 },
-    { userId: 3, balance: 240 },
-    { userId: 4, balance: 360 }
-];
+const name = prompt("Введіть ім'я");
+const surname = prompt("Введіть фамілію");
 
-const ans = users.reduce((totalBalance, { balance }) => totalBalance + balance, 0);
-console.log(ans);
+const email = name.concat(surname).toLowerCase() + "@gmail.com";
+console.log(email); 
+
--- js/script2.js
+++
@@ -1,9 +0,0 @@
-// Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends).
-
-const users = [
-    { name: "Max", friends: 4 },
-    { name: "Andriy", friends: 6 },
-    { name: "Mykola", friends: 2 },
-];
-const ans = users.sort((first, second) => first.friends - second.friends).reduce((acc, {name}) => [...acc, name], []);
-console.log(ans);
